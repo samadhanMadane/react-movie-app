cb1f3cdff3b6813b6f7fac82df79220c5e4dc822
Samadhan Madane <GS-1462@GSLAB.COM>
2018-07-17 18:22:03 +0530

    css modifications

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -31,88 +31,92 @@ class MovieDetails extends Component {
         console.log("render",this.movieDetails);
         return (
             <div className="movieDoc-details">
-            <div className="other-details">
-               <img className="movies-poster" src={this.movieDetails.Poster}/>
-               <div className="director">
-               <span className="movie-headers">Director : </span>{this.movieDetails.Director}
+                <div className="other-details">
+                    <img className="movies-poster" src={this.movieDetails.Poster}/>
+                    <div className="director">
+                        <span className="movie-headers">Director : </span>{this.movieDetails.Director}
                     </div>
                     <div className="director">
-                    <span className="movie-headers">Production : </span>{this.movieDetails.Production}</div>
+                        <span className="movie-headers">Production : </span>{this.movieDetails.Production}
+                    </div>
                     <div className="director">
-                    <span className="movie-headers">Response : </span>{this.movieDetails.Response}</div>
+                        <span className="movie-headers">Response : </span>{this.movieDetails.Response}
+                    </div>
                </div> 
                <div className="movie-details">
-               <div className="movie-name">
-                  {this.movieDetails.Title}
-                </div>
-             
-                <div className="type">
-                <span className="movie-language">
-                  {this.movieDetails.Language}
-                </span>
-                <span className="movie-type">
-                  {this.movieDetails.Type}
-                </span>
-                <span className="movie-genre">
-                  {this.movieDetails.Genre}
-                  </span>
-                  </div>
-                  <div>
-                  {/* Year : {this.props.movieDetails.Year}<br/> */}
-                  </div>
-                  <div className="type">
-                      <span className="release-date">
-                  <i className="fa fa-calendar"/>&nbsp;&nbsp; {this.movieDetails.Released}
-                  </span>
-                  <span className="release-time">
-                  <i className="fa fa-clock-o"/>&nbsp;&nbsp; {this.movieDetails.Runtime}
-                  </span>
-                  </div>
-                
-                  {/* Metascore : {this.props.movieDetails.Metascore}<br/> */}
-                  {/* Ratings : {this.props.movieDetails.Ratings}<br/> */}
-                  {/* Rated :  {this.props.movieDetails.Rated}<br/> */}
-                  {/* <h3 className="movie-synopsis">Synopsis:</h3> */}
-                  <p className="movie-plot">
-                  <span className="movie-headers">Synopsis</span>
-                      <p>{this.movieDetails.Plot}</p>
-                  </p>
-                  <div className="type-detail">
-                  <div className="movie-actors">
-                  <span className="movie-headers">Actors : </span>
-                  {this.movieDetails.Actors}
-                  </div>
-                  <div className="movie-actors">   
-                    <span className="movie-headers">Writer : </span>{this.movieDetails.Writer}</div>
-                  <div className="movie-actors">
-                  <span className="movie-headers">Rating : </span>
-                  {this.movieDetails.imdbRating} / 10
-                  </div>
-                  <div className="movie-actors">
-                  <span className="movie-headers">Votes : </span>
-                  {this.movieDetails.imdbVotes}
-                  </div>
-                  <div className="movie-actors">
-                  <span className="movie-headers">Website : </span>
-                  <a href={this.movieDetails.Website} target="_blank">{this.movieDetails.Website}</a>
-                  </div>
-                  <div className="movie-actors">
-                  <span className="movie-headers">Awards : </span>
-                  {this.movieDetails.Awards}
-                  </div>
-                  <div className="movie-actors">
-                  <span className="movie-headers">Box Office : </span>
-                  {this.movieDetails.BoxOffice}
-                  </div>
-                  <div className="movie-actors">
-                  <span className="movie-headers">Country : </span>
-                  {this.movieDetails.Country}
-                  </div>
-                  <div className="movie-actors">
-                  <span className="movie-headers">DVD : </span>
-                  {this.movieDetails.DVD}
-                  </div>
-                  </div>
+                    <div className="movie-name">
+                        {this.movieDetails.Title}
+                    </div>
+                    
+                    <div className="type">
+                        <span className="movie-language">
+                            {this.movieDetails.Language}
+                        </span>
+                        <span className="movie-type">
+                            {this.movieDetails.Type}
+                        </span>
+                        <span className="movie-genre">
+                            {this.movieDetails.Genre}
+                        </span>
+                    </div>
+                    <div>
+                        {/* Year : {this.props.movieDetails.Year}<br/> */}
+                    </div>
+                    <div className="type">
+                        <span className="release-date">
+                            <i className="fa fa-calendar"/>&nbsp;&nbsp; {this.movieDetails.Released}
+                        </span>
+                        <span className="release-time">
+                            <i className="fa fa-clock-o"/>&nbsp;&nbsp; {this.movieDetails.Runtime}
+                        </span>
+                    </div>
+                        {/* Metascore : {this.movieDetails.Metascore}<br/>
+                        Ratings : {this.movieDetails.Ratings}<br/>
+                        Rated :  {this.movieDetails.Rated}<br/> */}
+                    <p className="movie-plot">
+                        <span className="movie-headers">Synopsis</span>
+                            <p>{this.movieDetails.Plot}</p>
+                    </p>
+                    <div className="type-detail">
+                        <div className="movie-actors">
+                            <span className="movie-headers">Actors : </span>
+                            {this.movieDetails.Actors}
+                        </div>
+                        <div className="movie-actors">   
+                            <span className="movie-headers">Writer : </span>
+                            {this.movieDetails.Writer}
+                        </div>
+                        <div className="movie-actors">
+                            <span className="movie-headers">Rating : </span>
+                            {this.movieDetails.imdbRating} / 10
+                        </div>
+                        <div className="movie-actors">
+                            <span className="movie-headers">Votes : </span>
+                            {this.movieDetails.imdbVotes}
+                        </div>
+                        <div className="movie-actors">
+                            <span className="movie-headers">Website : </span>
+                            <a href={this.movieDetails.Website} target="_blank">
+                                {this.movieDetails.Website}
+                            </a>
+                        </div>
+                        <div className="movie-actors">
+                            <span className="movie-headers">Awards : </span>
+                            {this.movieDetails.Awards}
+                        </div>
+                        <div className="movie-actors">
+                            <span className="movie-headers">Box Office : </span>
+                            {this.movieDetails.BoxOffice}
+                        </div>
+                        <div className="movie-actors">
+                            <span className="movie-headers">Country : </span>
+                            {this.movieDetails.Country}
+                        </div>
+                        <div className="movie-actors">
+                            <span className="movie-headers">DVD : </span>
+                            {this.movieDetails.DVD}
+                        </div>
+                    </div>
                 </div>
             </div>
         );  
